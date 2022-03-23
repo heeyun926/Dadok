f1fab639ce9052def9d743704a13365b8006bdea
heeyun926 <fkffkfk31@gmail.com>
2022-03-23 18:18:11 +0900

    splash develop! - splash,main -> fragment - viewModel

--- app/src/main/java/com/example/dadok/MainActivity.kt
+++ app/src/main/java/com/example/dadok/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.dadok
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
     }
 }
