e5f7653fcefc40742449b43884f56a9892ecafaf
heeyun926 <90951683+heeyun926@users.noreply.github.com>
2022-03-28 03:49:56 +0900

    Retrofit api learning

--- app/src/main/java/com/example/dadok/ui/MainActivity.kt
+++ app/src/main/java/com/example/dadok/ui/MainActivity.kt
@@ -4,11 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dadok.databinding.ActivityMainBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
